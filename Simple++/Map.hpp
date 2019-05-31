783a66d0d6cae39b2d25f0f47e3fd24bfdfa3396
Clement Gerber <gerber.clement@gmail.com>
2019-05-31 18:38:11 +0200

    Added the ability to iterate Map in Ascending order.
    Signed-off-by: Clement Gerber <gerber.clement@gmail.com>

--- Simple++/Map.hpp
+++ Simple++/Map.hpp
@@ -1422,24 +1422,108 @@ bool RBTree<I, T, Compare>::iterate( typename RBTree<I, T, Compare>::Iterator * 
 	}
 }
 
+template<typename I, typename T, typename Compare>
+bool RBTree<I, T, Compare>::iterateGreater( typename RBTree<I, T, Compare>::Iterator * it ) const {
+	auto node( ( *it ) );
+
+	// While there is still data bigger, go to it.
+	if (node->getRight()) {
+		while (node->getLeft()) {
+			node = node->getLeft();
+		}
+		(*it) = node;
+		return true;
+
+	}
+	else {
+		// Ok, no more greater at this stage, let's go back.
+		while (true) {
+			if (node->getParent()) {
+				// Check if the current node come from left or right.
+				if (node == node->getParent()->getLeft()) {
+					// We came from left. This mean the parent this greater, fine !
+					(*it) = node;
+					return true;
+				}
+				else {
+					// We came from right. This mean the parent is smaller, go back one more time.
+					node = node->getParent();
+				}
+				
+			}
+			else {
+				// No parent, look's like we finished here.
+				(*it) = NULL;
+				return false;
+			}
+		}
+	}
+	// Should never access here.
+	return false;
+}
+
+template<typename I, typename T, typename Compare>
+bool RBTree<I, T, Compare>::iterateGreater(typename RBTree<I, T, Compare>::Iterator* it, I** i, T** v) const {
+	auto node((*it));
+	if (node == NULL)
+		return false;
+
+	(*i) = &(node->getValue()->getIndex());
+	(*v) = &(node->getValue()->getValue());
+
+	iterateGreater(it);
+	return true;
+}
+
+template<typename I, typename T, typename Compare>
+template<typename TestFunctor>
+bool RBTree<I, T, Compare>::iterateGreater(typename RBTree<I, T, Compare>::Iterator* it, I** i, T** v, TestFunctor& testFunctor) const {
+	auto node((*it));
+	if (node == NULL)
+		return false;
+
+	(*i) = &(node->getValue()->getIndex());
+	(*v) = &(node->getValue()->getValue());
+
+	if (testFunctor((*v))) {
+		iterateGreater(it);
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 
 template<typename I, typename T, typename Compare>
 typename RBTree<I, T, Compare>::Iterator RBTree<I, T, Compare>::getBegin() const {
 	return this -> rootNode;
 }
+
 template<typename I, typename T, typename Compare>
 typename RBTree<I, T, Compare>::Iterator RBTree<I, T, Compare>::begin() const {
 	return getBegin();
 }
+
 template<typename I, typename T, typename Compare>
 typename RBTree<I, T, Compare>::Iterator RBTree<I, T, Compare>::getEnd() const {
 	return NULL;
 }
+
 template<typename I, typename T, typename Compare>
 typename RBTree<I, T, Compare>::Iterator RBTree<I, T, Compare>::end() const {
 	return NULL;
 }
 
+template<typename I, typename T, typename Compare>
+typename RBTree<I, T, Compare>::Iterator RBTree<I, T, Compare>::getSmallest() const {
+	auto node(this->rootNode);
+
+	// Go Left until we can't anymore.
+	while (node->getLeft())
+		node = node->getLeft();
+
+	return node;
+}
 
 template<typename I, typename T, typename Compare>
 const T & RBTree<I, T, Compare>::getValueIt( typename RBTree<I, T, Compare>::Iterator it ) const {
